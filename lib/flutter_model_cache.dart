b0f40015328b4492aa920c9a2d55bb98e8f98fb1
Diarmuid Ryan <diarmuidr3d@gmail.com>
2025-07-20 21:09:49 +0100

    tidy

--- lib/flutter_model_cache.dart
+++ lib/flutter_model_cache.dart
@@ -1,6 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
 export 'src/generic_model_factory.dart';
@@ -8,5 +5,3 @@ export 'src/model.dart';
 export 'src/model_factory.dart';
 export 'src/model_list_stream.dart';
 export 'src/stream_state.dart';
-
-// TODO: Export any libraries intended for clients of this package.
